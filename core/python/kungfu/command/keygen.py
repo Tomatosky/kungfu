c511cccf7aad2a00b632fb648686f6fd43ca067d
Keren Dong <dongkeren@gmail.com>
2020-05-06 11:14:02 +0800

    Update keys generation

--- core/python/kungfu/command/keygen.py
+++ core/python/kungfu/command/keygen.py
@@ -1,13 +1,41 @@
 import click
+import sys
 from Crypto.PublicKey import RSA
+from PyInquirer import style_from_dict, Token, prompt
 from kungfu.command import kfc, pass_ctx_from_parent
 from kungfu.practice import write_key, KEY_PUB, KEY_PRI
+from pykungfu import yijinjing as yjj
+
+questions = [
+    {
+        'type': 'password',
+        'message': 'Enter password:',
+        'name': 'password'
+    }
+]
+
+custom_style = style_from_dict({
+    Token.Separator: '#6C6C6C',
+    Token.QuestionMark: '#FF9D00 bold',
+    Token.Selected: '#5F819D',
+    Token.Pointer: '#FF9D00 bold',
+    Token.Instruction: '',  # default
+    Token.Answer: '#5F819D bold',
+    Token.Question: '',
+})
 
 
 @kfc.command(help_priority=-1)
 @click.pass_context
 def keygen(ctx):
     pass_ctx_from_parent(ctx)
-    key = RSA.generate(2048)
-    write_key(ctx, key.export_key(pkcs=8), KEY_PRI)
-    write_key(ctx, key.publickey().export_key(pkcs=8), KEY_PUB)
+    answers = prompt(questions, style=custom_style)
+    hash_pass = yjj.hash_str_32(answers['password'])
+    if hash_pass == 112094729:
+        key = RSA.generate(2048)
+        write_key(ctx, key.export_key(pkcs=8), KEY_PRI)
+        write_key(ctx, key.publickey().export_key(pkcs=8), KEY_PUB)
+        click.echo(f'Keys generated at {ctx.home}')
+    else:
+        click.echo('Invalid password!')
+        sys.exit(-1)
--- core/python/kungfu/command/td.py
+++ core/python/kungfu/command/td.py
@@ -21,8 +21,9 @@ def td(ctx, source, account, low_latency):
     ext = EXTENSION_REGISTRY_TD.get_extension(source)(low_latency, ctx.locator, account, config.value)
     td_config = json.loads(config.value)
     account_id = td_config['account_id']
-    account_code = td_config['account_code']
-    if account_id == decode_text(ctx, account_code):
+    if td_home.group == 'sim':
+        ext.run()
+    elif 'account_code' in td_config and account_id == decode_text(ctx, td_config['account_code']):
         ext.run()
     else:
         logger = log.create_logger(account_id, ctx.log_level, td_home)
