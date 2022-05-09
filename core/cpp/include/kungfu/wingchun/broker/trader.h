5fecd9bf0c46a504a18ad404da6bdf1073105835
kfkev <kevcqu@qq.com>
2022-05-09 15:17:03 +0800

    [fix] modify req_history_order and req_history_trade not as pure virtual

--- core/cpp/include/kungfu/wingchun/broker/trader.h
+++ core/cpp/include/kungfu/wingchun/broker/trader.h
@@ -37,9 +37,9 @@ public:
 
   virtual bool req_account() = 0;
 
-  virtual bool req_history_order(const event_ptr &event) = 0;
+  virtual bool req_history_order(const event_ptr &event) { return true; }
 
-  virtual bool req_history_trade(const event_ptr &event) = 0;
+  virtual bool req_history_trade(const event_ptr &event) { return true; }
 
 protected:
   OrderMap orders_ = {};
