529ac645ac85074b0b61b621efdba5cd14f0f285
zhouran <zhouran@SERVER-101.directory.kungfu-trader.net>
2022-05-15 06:05:48 +0800

    [fix] add biz_index for entrust and transaction

--- core/cpp/include/kungfu/longfist/types.h
+++ core/cpp/include/kungfu/longfist/types.h
@@ -289,7 +289,8 @@ KF_DEFINE_PACK_TYPE(                                                    //
     (PriceType, price_type), //订单价格类型（市价、限价、本方最优）
 
     (int64_t, main_seq), //主序号
-    (int64_t, seq)      //子序号
+    (int64_t, seq),      //子序号
+    (int64_t, biz_index)  // 业务序号
 );
 
 KF_DEFINE_PACK_TYPE(                                                        //
@@ -314,7 +315,8 @@ KF_DEFINE_PACK_TYPE(                                                        //
     (BsFlag, bs_flag),     // SH: 内外盘标识
 
     (int64_t, main_seq), //主序号
-    (int64_t, seq)       //子序号
+    (int64_t, seq),      //子序号
+    (int64_t, biz_index)  // 业务序号
 );
 
 KF_DEFINE_PACK_TYPE(                                                 //
